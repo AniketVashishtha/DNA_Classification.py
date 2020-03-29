be20ef458fd9eb9ca751842883a5a5abfd489ea2
AniketVashishtha <53573925+AniketVashishtha@users.noreply.github.com>
2020-03-29 18:37:26 +0530

    Add files via upload

---
+++ DNA_classification.py
@@ -0,0 +1,102 @@
+import sys
+import numpy as np
+import sklearn
+import pandas as pd
+
+# import the uci Molecular Biology (Promoter Gene Sequences) Data Set
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/molecular-biology/promoter-gene-sequences/promoters.data'
+names = ['Class', 'id', 'Sequence']
+data = pd.read_csv(url, names = names)
+
+classes = data.loc[:, 'Class']
+print(classes[:5])
+
+# generate list of DNA sequences
+sequences = list(data.loc[:, 'Sequence'])
+dataset = {}
+
+# loop through sequences and split into individual nucleotides
+for i, seq in enumerate(sequences):
+    
+    # split into nucleotides, remove tab characters
+    nucleotides = list(seq)
+    nucleotides = [x for x in nucleotides if x != '\t']
+    
+    # append class assignment
+    nucleotides.append(classes[i])
+    
+    # add to dataset
+    dataset[i] = nucleotides
+    
+print(dataset[0])
+
+# turn dataset into pandas DataFrame
+dframe = pd.DataFrame(dataset)
+print(dframe)
+
+# Use the model_selection module to separate training and testing datasets
+from sklearn import model_selection
+
+# Create X and Y datasets for training
+X = np.array(df.drop(['Class'], 1))
+y = np.array(df['Class'])
+
+# define seed for reproducibility
+seed = 1
+
+# split data into training and testing datasets
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.25, random_state=seed)
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, accuracy_score
+
+# define scoring method
+scoring = 'accuracy'
+
+# Define models to train
+names = ["Nearest Neighbors", "Gaussian Process",
+         "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
+         "Naive Bayes", "SVM Linear", "SVM RBF", "SVM Sigmoid"]
+
+classifiers = [
+    KNeighborsClassifier(n_neighbors = 3),
+    GaussianProcessClassifier(1.0 * RBF(1.0)),
+    DecisionTreeClassifier(max_depth=5),
+    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+    MLPClassifier(alpha=1),
+    AdaBoostClassifier(),
+    GaussianNB(),
+    SVC(kernel = 'linear'), 
+    SVC(kernel = 'rbf'),
+    SVC(kernel = 'sigmoid')
+]
+
+models = zip(names, classifiers)
+
+# evaluate each model in turn
+results = []
+names = []
+
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=10, random_state = seed)
+    cv_results = model_selection.cross_val_score(model, X_train, y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+    
+for name, model in models:
+    model.fit(X_train, y_train)
+    predictions = model.predict(X_test)
+    print(name)
+    print(accuracy_score(y_test, predictions))
+    print(classification_report(y_test, predictions))
+    
+    
